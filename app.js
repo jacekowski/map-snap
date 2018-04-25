419ee2b02be51de9ccd405b9358b3720168849e5
Arel English <arelenglish@gmail.com>
2018-04-25 18:34:57 -0400

    switch to ejs because I hate jade

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
